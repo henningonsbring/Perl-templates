b90bb63a050771f896d4d616f924d934f7d403aa
Henning Onsbring <66672810+henningonsbring@users.noreply.github.com>
2020-09-04 16:57:24 +0200

    Update multithreading_template.pl

--- multithreading_template.pl
+++ multithreading_template.pl
@@ -5,18 +5,21 @@ use strict;
 use warnings;
 use threads;
 
-my $n_threads = 4;
+my $n_threads = 4; # How many processes that will be done in parallel
 my @thr = ();
 
+# Loop that starts multiple tasks in parallel on separate threads
 for (my $i = 0; $i < $n_threads; $i++) {
     $thr[$i] = threads->create({'context' => 'list'}, \&generic_sub, $i);
 }
 
+# To return calculations from threads
 for (my $i = 0; $i < $n_threads; $i++) {
     my ($returned_tid, $returned_sum) = $thr[$i]->join();
     print "The thread with thread ID " . $returned_tid . " got the sum " . $returned_sum . "\n";
 }
 
+# Subroutine that is done multiple times in parallel
 sub generic_sub {
     my ($thread_id) = @_;
     print "Thread " . $thread_id . " started" . "\n";
